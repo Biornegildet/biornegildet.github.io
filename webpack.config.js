85edd2f6ab0a09749fc278f080d19ad7da6599c1
peterhpo <peter.hjelle.petersenoeverleir@gmail.com>
2024-11-01 13:32:22 +0100

    Fixed bug with having the page running not on root-path

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,24 @@
 const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const fs = require('fs');
+
+// Load environment variables from the specified file
+const envFile = path.resolve(__dirname, 'baseurl.txt');
+const envContent = fs.readFileSync(envFile, 'utf-8');
+
+// Parse environment variables from the file content
+const env = envContent.split('\n').reduce((acc, line) => {
+  const [key, value] = line.split('=');
+  if (key && value) {
+    acc[key.trim()] = value.trim();
+  }
+  return acc;
+}, {});
+
+// Custom function to convert JavaScript values to Sass values
+function jsToSassString(value) {
+  return new (require('sass').types.String)(value);
+}
 
 module.exports = {
   mode: 'production',
@@ -28,6 +47,19 @@ module.exports = {
             loader: 'sass-loader',
             options: {
               sourceMap: true,
+              sassOptions: {
+                functions: {
+                  "env($variable)": variable => {
+                    const key = variable.getValue().toUpperCase();
+                    const value = env[key];
+                    if (value) {
+                      return jsToSassString(value);
+                    } else {
+                      throw new Error(`Environment variable ${key} not found`);
+                    }
+                  }
+                }
+              }
             },
           },
         ]
@@ -37,6 +69,6 @@ module.exports = {
   plugins: [
     new MiniCssExtractPlugin({
       filename: '[name].css',
-    }),
+    })
   ]
 };
